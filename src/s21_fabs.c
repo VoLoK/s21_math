ee50b2d76587340134f04a947feb78ac0c944ac4
Tanya <112762382+kireeva8@users.noreply.github.com>
2023-06-30 19:29:59 +0300

    Update s21_fabs.c

--- src/s21_fabs.c
+++ src/s21_fabs.c
@@ -1,13 +1,5 @@
 #include "s21_math.h"
 
 long double s21_fabs(double x) {
-//   double res;
-//   if (x > 0) {
-//     res = x;
-//   } else {
-//     res = x * -1.0;
-//   }
-//   return res;
-
   return (x > 0) ? x : -x;
 }
