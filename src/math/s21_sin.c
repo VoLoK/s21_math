5354070b9f8ace77af21a78ce6da7fc2f08e5827
Lawnties <99724334+tinskrin@users.noreply.github.com>
2023-06-30 21:12:41 +0300

    Update s21_sin.c

--- src/math/s21_sin.c
+++ src/math/s21_sin.c
@@ -1,9 +1,8 @@
 #include "s21_math.h"
-#include <math.h>
 
 long double s21_sin(double x) {
     long double res = 0;
-    x = fmod(x, 2 * s21_PI);
+    x = s21_fmod(x, 2 * s21_PI);
     for (int i = 0; i < 6; i++) {
         res += (s21_pow(-1, i) * s21_pow(x, 2 * i + 1)) / (s21_fact(2 * i + 1));
     }
