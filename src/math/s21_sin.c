de2e1e31c81263aa8bed06691aa431989ba7e3b0
Lawn Tiesha <lawnties@student.21-school.ru>
2023-06-30 21:09:25 +0300

    done tests

--- src/math/s21_sin.c
+++ src/math/s21_sin.c
@@ -1,8 +1,9 @@
 #include "s21_math.h"
+#include <math.h>
 
 long double s21_sin(double x) {
     long double res = 0;
-    x = s21_fmod(x, 2 * s21_PI);
+    x = fmod(x, 2 * s21_PI);
     for (int i = 0; i < 6; i++) {
         res += (s21_pow(-1, i) * s21_pow(x, 2 * i + 1)) / (s21_fact(2 * i + 1));
     }
